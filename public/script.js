a2d81c59eb801271746b248f6507d44ab8be5645
Daniel Ventura <ventura.daniel36@gmail.com>
2020-02-19 21:32:42 -0700

    integrated api and added styling

--- public/script.js
+++ public/script.js
@@ -17,7 +17,29 @@ weatherButton.addEventListener('click', () =>{
         }
 })
 .then(res => res.json())
-.then(json => console.log(json))
+.then(data => {setWeatherData(data, searchElementText)})
+.catch(error =>{
+    console.log(error)
+})
  
 })
+
+const icon = new Skycons({color: '#222'})
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temperatureElement = document.querySelector('[data-temperature]')
+const precipitationElement = document.querySelector('[data-precipitation]')
+const windElement = document.querySelector('[data-wind]')
+icon.set('icon', 'clear-day')
+icon.play()
+
+function setWeatherData(data, place){
+    locationElement.textContent = place
+    statusElement.textContent = data.summary
+    temperatureElement.textContent = data.temperature
+    precipitationElement.textContent = `${data.precipProbability * 100}%`
+    windElement.textContent = data.windSpeed
+    icon.set('icon', data.icon)
+    icon.play()
+}
 
--- server.js
+++ server.js
@@ -7,22 +7,41 @@ const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY
 const express = require('express')
 const app = express()
 const opencage = require('opencage-api-client')
-
+const axios = require('axios')
+let latitude = ''
+let longitude = ''
+let url =''
 app.use(express.json())
 app.use(express.static('public'))
 
 
+
+
 app.post('/weather', (req , res) =>{
-    console.log(req.body)
-
-
+    
     opencage.geocode(req.body).then(data => {
     // console.log(JSON.stringify(data));
     if (data.status.code == 200) {
         if (data.results.length > 0) {
         var place = data.results[0];
-        console.log(place.geometry.lat);
-        console.log(place.geometry.lng)
+        latitude = place.geometry.lat;
+        longitude = place.geometry.lng;
+        url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${latitude},${longitude}?units=auto`
+
+        const getWeather = async () => {
+          try {
+            return await axios.get(url)
+          } catch (error) {
+            console.error(error)
+          }
+        }
+        const weatherData = async () => {
+          const weather = await getWeather()
+          res.json(weather.data.currently)
+        }
+        
+        weatherData()
+
         }
     } else if (data.status.code == 402) {
         console.log('hit free-trial daily limit');
@@ -34,7 +53,9 @@ app.post('/weather', (req , res) =>{
     }
     }).catch(error => {
     console.log('error', error.message);
-    });
+     })//.then(data => res.json(data.data.currently))
+    
+
 })
 
 
